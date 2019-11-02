5eec70e1550e198e0eb88d815fb7bff18df7368e
[magdalena almanova] <[magdaalmanova@gmail.com]>
2019-11-02 15:17:01 +0100

    app.py pokrok

--- app.py
+++ app.py
@@ -7,11 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     print(get_activities())
-    return render_template('home.html')
-
-@app.route('/about/')
-def about():
-    return render_template('about.html')
+    return render_template('index.html')
 
 @app.route('/ranapece/')
 def ranapece():
