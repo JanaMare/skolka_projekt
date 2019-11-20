703f2a2900b28e50d8ea441dec493b433e2c5757
Meguska <55955266+meguska@users.noreply.github.com>
2019-11-20 22:47:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ csrf.init_app(app)
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'specialniskolky@gmail.com'
-app.config['MAIL_PASSWORD'] = 'MagdusJana'
+app.config['MAIL_PASSWORD'] = '################'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = True
 
